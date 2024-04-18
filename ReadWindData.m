1e645a3bc9de1cece83eeef54d57365e12ef796a
linfengYang <eidp_gxu@126.com>
2024-04-17 17:31:40 -0700

    Update ReadWindData.m

--- ReadWindData.m
+++ ReadWindData.m
@@ -1,9 +1,9 @@
 %%  ReadWindData  by yy 2021.8.27
 function [data_Wind] = ReadWindData(data,Filedir)
 
-wind_num = data.Wind.wind_Number; %风电机组个数
+wind_num = data.Wind.wind_Number; %number of wind units
 
-wind = xlsread(Filedir); %读取风力数据
+wind = xlsread(Filedir); %read wind data
 data = wind(1:end,2:end);
 data = data(:,9:8+wind_num);
 data = rmmissing(data,1);
